bf471725e9191aa1559b9dd97e54ffb20fe98ef9
mdemena <mdemena@gmail.com>
2020-09-01 21:27:22 +0200

    Google API Fix

--- configs/passport.config.js
+++ configs/passport.config.js
@@ -51,7 +51,7 @@ passport.use(
 		{
 			clientID: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-			callbackURL: '/auth/google/callback',
+			callbackURL: '/api/auth/google/callback',
 		},
 		(accessToken, refreshToken, profile, done) => {
 			// to see the structure of the data in received response:
